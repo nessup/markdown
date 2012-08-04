7f27a1bc20f8317a91f9eb138985a1d88aabd6f1
Mattias Appelren <mattias@ppelgren.se>
2012-08-03 22:48:37 -0700

    Adding some DOM modification, taking screenshots in between

--- popup.js
+++ popup.js
@@ -2,14 +2,33 @@
 
 
 function capture() {
-	alert("capturing");
- 	chrome.tabs.captureVisibleTab(null, function(dataUrl) {
+	//alert("capturing");
+    //alert($(document).hmtl())
+ 	chrome.tabs.captureVisibleTab(null, {format: "png"}, append);
+    chrome.tabs.executeScript(null, {code:"document.body.bgColor='red'"});
+    sleep(1000)
+ 	chrome.tabs.captureVisibleTab(null, {format: "png"}, append);
+    chrome.tabs.executeScript(null, {code:"document.body.bgColor='blue'"});
+    sleep(1000)
+ 	chrome.tabs.captureVisibleTab(null, {format: "png"}, append);
+};
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+function append(dataUrl){
 	  content = document.getElementById('screenshots');
 	  var image = document.createElement('img');
 	  image.setAttribute('src', dataUrl);
 	  content.appendChild(image);
-    });
-};
+}
+
 
 
    $("#capture").click(capture());
